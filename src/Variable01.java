1a77237bd81c714b40e206dc9a1f429c19659398
hanada <hanada@localhost>
2023-11-18 23:24:23 +0900

    Lesson2_Chapter5終了

---
+++ src/Variable01.java
@@ -0,0 +1,19 @@
+
+
+public class Variable01 {
+	public static void main(String[] args) {
+		// 「String」型の変数を宣言し、自分の名前を代入
+		String name = "ウェブ　キャンプ之助";
+		// 自分の名前を出力
+		System.out.println(name);
+		// 「String」型の変数に、好きな言葉を再代入
+		name = "少年よ大志を抱け";
+		// 好きな言葉を出力
+		System.out.println(name);
+
+		// 「int」型の定数を宣言し、ラッキーナンバーを代入
+		final int luckyNumber = 7;
+		// ラッキーナンバーを出力
+		System.out.println(luckyNumber);
+	}
+}
