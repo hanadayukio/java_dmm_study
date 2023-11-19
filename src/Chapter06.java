e7f1d1798caee0ad6bbe066521fcf8b0a3986927
hanadayukio <124165299+hanadayukio@users.noreply.github.com>
2023-11-19 15:35:06 +0900

    Chapter06,07終了

---
+++ src/Chapter06.java
@@ -0,0 +1,27 @@
+
+public class Chapter06 {
+	
+	public static void main(String[] args) {
+		long long_p = 10000000000L;
+		System.out.println(long_p);
+		
+		double duble_p =  3.14d;
+		System.out.println(duble_p);
+		
+		boolean boolean_p =  true;
+		System.out.println(boolean_p);
+		
+		Integer integer_w = 100;
+		System.out.println(integer_w);
+		
+		Float float_w = 1.4142F;
+		System.out.println(float_w);
+		
+		Character character_w = 'う';
+		System.out.println(character_w);
+		
+		int[] array = {0,10,20,30,40};
+		System.out.println(array[3]);
+	}
+
+}
---
+++ src/Chapter07.java
@@ -0,0 +1,69 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		
+		int val = 2 + 5;
+		System.out.println(val);
+		
+		val++;
+		System.out.println(val);
+		
+		val *= 5;
+		System.out.println(val);
+		
+		boolean bool;
+		bool = val == 50;
+		System.out.println(bool);
+		
+		bool = (val < 50) && (val % 10 == 0);
+		System.out.println(bool);
+		
+		
+		
+		
+		
+		
+		
+		
+////	演算子の種類と使い方
+//		int a;
+//		int b;
+//		boolean c;
+//		a = 6;
+//		b = 3;
+//		
+////		a = a + b と同じ
+//		a += b;
+//		System.out.println(a);
+////		a = a - b と同じ
+//		a -= b;
+//		System.out.println(a);
+////		a = a * bと同じ
+//		a *= b;
+//		System.out.println(a);
+////		a = a / bと同じ
+//		a /= b;
+//		System.out.println(a);
+////		a = a % bと同じ
+//		a %= b;
+//		System.out.println(a);
+////		a が b より小さい場合に true
+//		c = a < b;
+//		System.out.println(c);
+////		a が b より小さいか等しい場合に true
+//		c = a <= b;
+//		System.out.println(c);
+////		a が b より大きい場合に true
+//		c = a > b;
+//		System.out.println(c);
+////		a が b より大きいか等しい場合に true
+//		c = a >= b;
+//		System.out.println(c);
+////		a と b が等しい場合に true
+//		c = a == b;
+//		System.out.println(c);
+////		a と b が等しくない場合に true
+//		c = a != b;
+//		System.out.println(c);
+	}
+}
