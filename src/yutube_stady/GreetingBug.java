f3992150742318fcfa798f52213fbc9e733175d7
hanadayukio <124165299+hanadayukio@users.noreply.github.com>
2024-03-04 23:32:16 +0900

    2-5_演習問題

---
+++ src/yutube_stady/GreetingBug.java
@@ -0,0 +1,8 @@
+package yutube_stady;
+
+public class GreetingBug {
+	public static void main(String[] args) {
+		System.out.println("こんにちは。");
+		System.out.println("一緒に頑張りましょう！");
+	}
+}
---
+++ src/yutube_stady/ShowData.java
@@ -0,0 +1,22 @@
+package yutube_stady;
+
+public class ShowData {
+	public static void main(String[] args) {
+//		変数の宣言と代入
+		int num = 10;
+		double pi = 3.14;
+		char blood = 'A';
+		boolean fun = true;
+		String msg = "hallo";
+		
+		System.out.println(num); 
+		System.out.println(pi);
+		System.out.println(fun);
+		System.out.println(msg);
+		System.out.println(blood);
+		
+//		値の表示
+	}
+	
+
+}
